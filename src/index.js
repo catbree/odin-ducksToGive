39a1051bd85bef817c8db6a9eae238cd9a2bc87c
Hoi Cheng <49464204+catbree@users.noreply.github.com>
2024-04-05 04:53:56 +0800

    added a html-loader to load images on html

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import './style.css';
+import html from "./index.html";
 import printMe from './navigations.js';
 
 function component() {
     const element = document.createElement('div');
-  
-    // Lodash, currently included via a script, is required for this line to work
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   
     return element;
   }
--- webpack.config.js
+++ webpack.config.js
@@ -29,11 +29,15 @@ module.exports = {
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
+        type: 'asset/resource'
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
         type: 'asset/resource',
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
       },
     ],
   },
