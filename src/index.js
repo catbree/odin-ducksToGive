378d9de4541dcf494aeb656e24fec7e4133912a3
Hoi Cheng <49464204+catbree@users.noreply.github.com>
2024-04-05 04:15:17 +0800

    installed webpack and some dependencies

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import './style.css';
+import printMe from './navigations.js';
+
+function component() {
+    const element = document.createElement('div');
+  
+    // Lodash, currently included via a script, is required for this line to work
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  
+    return element;
+  }
+  
+  document.body.appendChild(component());
+
+  printMe();
