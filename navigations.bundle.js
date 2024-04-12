8291e32b84adc0b32327682c778d334fba12490a
Hoi Cheng <49464204+catbree@users.noreply.github.com>
2024-04-12 17:48:35 +0800

    initial dist subtree commit

---
+++ navigations.bundle.js
@@ -0,0 +1,26 @@
+"use strict";
+(self["webpackChunkodin_duckstogive"] = self["webpackChunkodin_duckstogive"] || []).push([["navigations"],{
+
+/***/ "./src/navigations.js":
+/*!****************************!*\
+  !*** ./src/navigations.js ***!
+  \****************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (/* binding */ printMe)
+/* harmony export */ });
+function printMe() {
+    console.log('I get called from navigation.js!');
+  }
+
+/***/ })
+
+},
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ var __webpack_exports__ = (__webpack_exec__("./src/navigations.js"));
+/******/ }
+]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibmF2aWdhdGlvbnMuYnVuZGxlLmpzIiwibWFwcGluZ3MiOiI7Ozs7Ozs7Ozs7Ozs7QUFBZTtBQUNmO0FBQ0EiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9vZGluLWR1Y2tzdG9naXZlLy4vc3JjL25hdmlnYXRpb25zLmpzIl0sInNvdXJjZXNDb250ZW50IjpbImV4cG9ydCBkZWZhdWx0IGZ1bmN0aW9uIHByaW50TWUoKSB7XG4gICAgY29uc29sZS5sb2coJ0kgZ2V0IGNhbGxlZCBmcm9tIG5hdmlnYXRpb24uanMhJyk7XG4gIH0iXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
